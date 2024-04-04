cf6f5f53790921989f4b970b135ff4f516ae2b8d
Mariia Shabelnik <mariiashabelnik@gmail.com>
2024-04-04 22:11:25 +0200

    added support for CRUD operations

--- src/index.js
+++ src/index.js
@@ -12,41 +12,83 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/api/games", async (req, res) => {
-  const games = await prisma.game.findMany();
+app.get("/api/gametype", async (req, res) => {
+  const games = await prisma.gameType.findMany();
   res.json({ status: true, data: games });
 });
 
-app.post("/api/games", async (req, res) => {
+app.post("/api/gametype", async (req, res) => {
   try {
-    const { game } = req.body;
-    await prisma.game.create({ data: { name: game } });
-    const games = await prisma.game.findMany();
+    const { gameType } = req.body;
+    await prisma.gameType.create({ data: { name: gameType } });
+    const games = await prisma.gameType.findMany();
     res.status(201).json({ status: true, data: games });
   } catch (error) {
-    res.status(405).json({ status: false, message: "Game already exists" });
+    res.status(405).json({ status: false, message: "Gametype already exists" });
   }
 });
 
-app.get("/api/gamerounds", async (req, res) => {
-  const games = await prisma.game.findMany();
-  res.json({ status: true, data: games });
+app.patch("/api/gametype/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    const updateGame = await prisma.gameType.update({
+      where: { id },
+      data: { name },
+    });
+    res.json({ updateGame });
+  } catch (error) {
+    res.json({ msg: "No such gametype", error });
+  }
 });
 
-app.post("/api/gamerounds", async (req, res) => {
+app.get("/api/gameround", async (req, res) => {
+  const gameRounds = await prisma.gameRound.findMany({
+    include: { gameType: true, players: true },
+  });
+  res.json({ status: true, data: gameRounds });
+});
+
+app.post("/api/gameround", async (req, res) => {
   try {
-    const { game } = req.body;
-    await prisma.game.create({ data: { name: game } });
-    const games = await prisma.game.findMany();
-    res.status(201).json({ status: true, data: games });
+    const { gameTypeId, players } = req.body;
+    const gameRound = await prisma.gameRound.create({
+      data: {
+        gameTypeId: gameTypeId,
+        players: { connect: players },
+      },
+    });
+    res.json({ status: true, data: gameRound });
   } catch (error) {
-    res.status(405).json({ status: false, message: "Game already exists" });
+    console.log(error);
   }
 });
 
 app.get("/api/players", async (req, res) => {
-  const players = await prisma.player.findMany();
-  res.json(players);
+  const players = await prisma.player.findMany({});
+  res.json({ status: true, data: players });
+});
+
+app.patch("/api/player/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  const updateUser = await prisma.player.update({
+    where: { id },
+    data: { name, email },
+  });
+  res.json({ updateUser });
+});
+
+app.delete("/api/player/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteUser = await prisma.player.delete({
+      where: { id },
+    });
+    res.json({ deleteUser });
+  } catch (error) {
+    res.json({ message: "No such user exists" });
+  }
 });
 
 app.post("/api/players", async (req, res) => {
